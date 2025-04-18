a9d822d658b8be14c0deed87d6da6346aa2e5c1b
charliejade8888 <charliejade8888@gmail.com>
2025-04-18 12:49:04 +0100

    added TODO on kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,14 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlinx.kover") version "0.7.4"
-    id("io.gitlab.arturbosch.detekt") version "1.23.1"
-    id("org.springframework.boot") version "3.4.0" // Already up-to-date
-    id("io.spring.dependency-management") version "1.1.3"
-    kotlin("plugin.spring") version "1.9.10"
-    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.22"
 }
-
-
+// TODO comment out kover/detekt, fix the rest then start from scratch via google/their docs!! - run build/boot first to find failure
 group = "com.tyrell"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -29,43 +28,36 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.json:json:20220924")
+    implementation("org.json:json:20250107")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-//    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.12")
-//    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
-
-    // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webflux-ui
-    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.0.2")
-
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
 
     implementation("org.mariadb:r2dbc-mariadb:1.1.3")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.liquibase:liquibase-core")
-    implementation("org.mariadb.jdbc:mariadb-java-client")
+    implementation("org.liquibase:liquibase-core:4.23.2")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.2.0")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-devtools")
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.testcontainers:testcontainers:1.17.5")
-    testImplementation("org.testcontainers:junit-jupiter:1.17.5")
-    testImplementation("org.testcontainers:mariadb:1.17.5")
-    testImplementation("org.testcontainers:r2dbc:1.17.5")
-    testImplementation("org.freemarker:freemarker:2.3.31")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.testcontainers:testcontainers:1.19.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.0")
+    testImplementation("org.testcontainers:mariadb:1.19.0")
+    testImplementation("org.testcontainers:r2dbc:1.19.0")
+    testImplementation("org.freemarker:freemarker:2.3.32")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.junit.vintage:junit-vintage-engine:5.9.0")
-
-    testImplementation("io.cucumber:cucumber-java:7.9.0")
-    testImplementation("io.cucumber:cucumber-spring:7.9.0")
-    testImplementation("io.cucumber:cucumber-junit:7.9.0")
-    testImplementation("io.rest-assured:rest-assured")
-    testImplementation("io.rest-assured:json-path")
-    testImplementation("io.rest-assured:xml-path")
+    testImplementation("io.cucumber:cucumber-java:7.15.0")
+    testImplementation("io.cucumber:cucumber-spring:7.15.0")
+    testImplementation("io.cucumber:cucumber-junit:7.15.0")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    testImplementation("io.rest-assured:json-path:5.3.0")
+    testImplementation("io.rest-assured:xml-path:5.3.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -74,10 +66,6 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "17"
     }
 }
-
-//tasks.withType<Test> {
-//    useJUnitPlatform()
-//}
 
 val SourceSet.kotlin: SourceDirectorySet
     get() = project.extensions.getByType<KotlinJvmProjectExtension>().sourceSets.getByName(name).kotlin
@@ -95,7 +83,6 @@ val componentTest = task<Test>("component-test") {
     group = "verification"
     testClassesDirs = sourceSets["component-test"].output.classesDirs
     classpath = sourceSets["component-test"].runtimeClasspath
-//    useJUnitPlatform()
     dependsOn("assemble")
 }
 
@@ -103,11 +90,14 @@ tasks.withType<Jar>() {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
-//https://kotlin.github.io/kotlinx-kover/
-koverReport {
-    verify {
-        rule {
-            minBound(0)
+kover {
+    coverage {
+        verify {
+            rule {
+                bound {
+                    minValue = 0
+                }
+            }
         }
     }
     filters {
